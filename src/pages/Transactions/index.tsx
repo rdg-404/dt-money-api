f050d3593c79b14db6aaf71f529606d12d918a13
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-05-05 23:26:01 -0300

    created summary section

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 export function Transations() {
   return(
     <div>
       <Header/>
+      <Summary/>
     </div>
   )
 }
