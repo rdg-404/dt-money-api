89d7de304c44772b887ec1654ed2d08e86229c75
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-05-06 00:06:59 -0300

    created table transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transations() {
   return(
     <div>
       <Header/>
       <Summary/>
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Densevolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">
+                  R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13-04-2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Games</td>
+              <td>
+                <PriceHighLight variant="outcome">
+                  -R$ 300,00
+                </PriceHighLight>
+              </td>
+              <td>Lazer</td>
+              <td>10-04-2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
