1f2071e32bbebc53a50b152c73379794db943ec2
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-05-10 12:34:36 -0300

    estiliza o modal - cria formulario e estiliza

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,14 +1,31 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import { CloseButton, Content, Overlay } from './style';
+import { X } from 'phosphor-react';
 
 
 export function NewTransactionModal(){
   return(
     <Dialog.Portal>
-      <Dialog.Overlay/>
-      <Dialog.Content>
+      <Overlay/>
+      <Content>
         <Dialog.Title>Nova Transação</Dialog.Title>
-        <Dialog.Close/>
-      </Dialog.Content>
+
+        <CloseButton>
+          <X size={24}/>
+        </CloseButton>
+
+        <form action="">
+          <input type="text" placeholder='Descrição' required />
+          <input type="number" placeholder='Preço' required />
+          <input type="text" placeholder='Categoria' required />
+
+          <button type='submit'>
+            Cadastrar
+          </button>
+        </form>
+
+       
+      </Content>
     </Dialog.Portal>
   )
 }
