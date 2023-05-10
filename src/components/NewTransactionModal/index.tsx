de881f374b8803b3732d54cb704906f3e0d70d95
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-05-10 12:55:05 -0300

    cria os botoes de entrada e saida e estiliza

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { CloseButton, Content, Overlay } from './style';
-import { X } from 'phosphor-react';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './style';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
 
 export function NewTransactionModal(){
@@ -18,6 +18,22 @@ export function NewTransactionModal(){
           <input type="text" placeholder='Descrição' required />
           <input type="number" placeholder='Preço' required />
           <input type="text" placeholder='Categoria' required />
+
+
+          <TransactionType>
+            <TransactionTypeButton>
+              <ArrowCircleUp size={24}/>
+              Entrada
+            </TransactionTypeButton>
+
+
+            <TransactionTypeButton>
+              <ArrowCircleDown size={24}/>
+              Saída
+            </TransactionTypeButton>
+
+          </TransactionType>
+
 
           <button type='submit'>
             Cadastrar
