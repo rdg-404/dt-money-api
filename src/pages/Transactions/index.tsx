52f84f59d3ac0a331b8d44114ceb34d8a461c89a
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-05-13 18:08:10 -0300

    formatando o preço para real BR e formatando a data para ptBR

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,6 +4,7 @@ import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter } from "../../utils/formatter";
 
 
 
@@ -26,11 +27,12 @@ export function Transations() {
                   <td width="50%">{transaction.description}</td>
                     <td>
                       <PriceHighLight variant={transaction.type}>
+                        {transaction.type === "outcome" && '- '}
                         {transaction.price}
                       </PriceHighLight>
                     </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             })}
