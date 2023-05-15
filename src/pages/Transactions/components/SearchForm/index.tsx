18c905994189e9d923c6901f7855aef4d71b7472
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-05-14 23:25:37 -0300

    configura o botao de buscar para ficar desativado enquanto envia as informaçoes

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -13,11 +13,17 @@ type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm(){
 
-  const {register, handleSubmit} = useForm<SearchFormInputs>({
+  const {
+    register, 
+    handleSubmit, 
+    formState: {isSubmitting}
+  } = useForm<SearchFormInputs>({
     resolver: zodResolver(searchFormSchema)
   })
 
-  function handleSearchTransactions(data: SearchFormInputs){
+  async function handleSearchTransactions(data: SearchFormInputs){
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
     console.log(data)
   }
 
@@ -29,7 +35,7 @@ export function SearchForm(){
         {...register('query')}
       />
 
-      <button type="submit">
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass/>
         Buscar
       </button>
--- src/pages/Transactions/components/SearchForm/style.ts
+++ src/pages/Transactions/components/SearchForm/style.ts
@@ -29,9 +29,15 @@ export const SearchFormContainer = styled.form`
     color: ${props => props.theme['green-300']};
     font-weight: bold;
     border-radius: 6px;
+    cursor: pointer;
 
 
-    &:hover{
+    &:disabled {
+      opacity: 0.5;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover{
       background: ${props => props.theme['green-500']};
       border-color: ${props => props.theme['green-500']};
       color: ${props => props.theme.white};
