eb1d90488fd9c866ffb63e157a868b9ff98282fe
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-05-14 23:17:59 -0300

    configurando zod para receber os dados enviados no campo de buscar

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,10 +1,33 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./style";
+import { useForm } from "react-hook-form";
+import * as z from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm(){
+
+  const {register, handleSubmit} = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema)
+  })
+
+  function handleSearchTransactions(data: SearchFormInputs){
+    console.log(data)
+  }
+
   return(
-    <SearchFormContainer>
-      <input type="" placeholder="Buscar por transações"/>
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input 
+        type="" 
+        placeholder="Buscar por transações"
+        {...register('query')}
+      />
 
       <button type="submit">
         <MagnifyingGlass/>
