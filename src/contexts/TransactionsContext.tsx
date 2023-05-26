56c87591a99c74e9b854a01853f05e67ce328648
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-05-26 17:27:10 -0300

    cria function add new data

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,10 +10,17 @@ interface Transaction {
   createdAt: string;
 }
 
+interface CreateTransactionInput {
+  description: string;
+  price: number;
+  category: string;
+  type: "income" | "outcome"
+}
 
 interface TransactionContextType {
   transactions: Transaction[];
   fetchTransactions: (query?: string) => Promise<void>;
+  createTransaction: (data: CreateTransactionInput) => Promise<void>;
 }
 
 
@@ -40,6 +47,19 @@ export function TransactionsProvider({ children }: TransactionsProviderProps){
     setTransactions(response.data)
   }
   
+  async function createTransaction(data: CreateTransactionInput){
+    const {description, price, category, type} = data
+
+    const response = await api.post('transactions', {
+      description,
+      price,
+      category,
+      type,
+      createdAt: new Date()
+  })
+  setTransactions(state => [response.data, ...state])
+}
+
 
   //executar somente uma vez a chamada para a api
   useEffect(() =>{
@@ -49,7 +69,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps){
 
   //children reconhece qualquer elemento html
   return (
-    <TransactionsContext.Provider value={{transactions, fetchTransactions}}>
+    <TransactionsContext.Provider value={{transactions, fetchTransactions, createTransaction}}>
       {children}
     </TransactionsContext.Provider>
   )
