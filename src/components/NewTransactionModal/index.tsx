5ebbde2cebed1b3f1a4022ebb6c3e5df65c280ac
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-05-26 17:28:05 -0300

    useContext refres app after add new data

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,7 +4,8 @@ import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 import * as z from 'zod'
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { api } from '../../lib/axios';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 
 const newTransactionFormSchema = z.object({
@@ -17,6 +18,8 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal(){
+
+  const {createTransaction} =  useContext(TransactionsContext)
 
   const {
     control,
@@ -33,15 +36,16 @@ export function NewTransactionModal(){
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs){
     const {description, price, category, type} = data
-    await api.post('transactions', {
-        description,
-        price,
-        category,
-        type,
-        createdAt: new Date()
+    await createTransaction({
+      description,
+      price,
+      category,
+      type
     })
     reset()
   }
+  
+
 
   return(
     <Dialog.Portal>
